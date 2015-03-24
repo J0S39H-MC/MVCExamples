91ad19a297ba3d7494631898eb86da81bf9f9b70
J0S39H-MC <josephmcclean@outlook.com>
2015-03-23 21:28:26 -0400

    mvc examples

---
+++ ProASPMvc4Examples/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+      Product myProduct = new Product { Name = "Biscuits", Categoty = "Food", Description = string.Empty, Price = 4.99m, ProductID = 1 };
+        
+      // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
